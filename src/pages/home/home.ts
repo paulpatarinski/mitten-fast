f6196a20ac8d338b65848df206888d57ee38785e
Paul Patarinski <paulpatarinski@gmail.com>
2018-02-18 14:58:38 -0600

    Include forward/back arrows

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,29 +1,62 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
 import moment from 'moment';
-import {Storage} from '@ionic/storage';
+import { Storage } from '@ionic/storage';
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
-  constructor(public navCtrl: NavController, private storage : Storage) {
+  constructor(public navCtrl: NavController, private storage: Storage) {
   }
 
+  startDate: Date;
+  currentlySelectedDate: Date;
+  selectedRecord: WeightRecord;
+
   ionViewDidLoad() {
+    this.currentlySelectedDate = new Date();
+
     this.storage.get('startDate').then((startDate) => {
+      this.startDate = startDate;
       const currentDateMoment = moment(new Date());
-      const startDateMoment =moment(startDate);
-      const daysSinceFastStart = currentDateMoment.diff(startDateMoment, 'days');
+      const startDateMoment = moment(startDate);
+      const daysSinceFastStart = currentDateMoment.diff(startDateMoment, 'days') + 1;
 
       this.selectedRecord = {
-        date : new Date(),
-        dayNum : daysSinceFastStart,
-        img : null
+        date: new Date(),
+        dayNum: daysSinceFastStart,
+        img: null
       };
     });
   }
 
-  public selectedRecord : WeightRecord;
+  isOnStartDay() {
+    if (!this.currentlySelectedDate)
+      return false;
+
+    const currentlySelectedDateMoment = moment(this.currentlySelectedDate);
+    const startDateMoment = moment(this.startDate);
+
+    return currentlySelectedDateMoment.diff(startDateMoment, 'days') === 0;
+  }
+
+  isOnLatestDay() {
+    if (!this.currentlySelectedDate)
+      return false;
+
+    const currentlySelectedDateMoment = moment(this.currentlySelectedDate);
+    const todayMoment = moment(new Date());
+
+    return currentlySelectedDateMoment.diff(todayMoment, 'days') === 0;
+  }
+
+  navDayBack() {
+    this.currentlySelectedDate = moment(this.currentlySelectedDate).subtract(1, 'day').toDate();
+  }
+
+  navDayForward() {
+    this.currentlySelectedDate = moment(this.currentlySelectedDate).add(1, 'day').toDate();
+  }
 }
--- src/pages/welcome/welcome.ts
+++ src/pages/welcome/welcome.ts
@@ -1,17 +1,19 @@
-import {Component} from '@angular/core';
-import {NavController} from 'ionic-angular';
-import {HomePage} from '../home/home';
-import {Storage} from '@ionic/storage';
+import { Component } from '@angular/core';
+import { NavController } from 'ionic-angular';
+import { HomePage } from '../home/home';
+import { Storage } from '@ionic/storage';
+import moment from 'moment';
 
-@Component({selector: 'page-welcome', templateUrl: 'welcome.html'})
+@Component({ selector: 'page-welcome', templateUrl: 'welcome.html' })
 export class WelcomePage {
 
-  constructor(public navCtrl : NavController, private storage : Storage) {}
+  constructor(public navCtrl: NavController, private storage: Storage) { }
 
   startClicked() {
+    const startDate = moment(new Date()).subtract(1, 'day').toDate();
     this
       .storage
-      .set('startDate', new Date())
+      .set('startDate', startDate)
       .then(() => {
         this
           .navCtrl
